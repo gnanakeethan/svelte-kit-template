9ae3b021214b8ae46ba03991ee4da01f1a101459
Gnanakeethan <gnanakeethan@users.noreply.github.com>
2021-05-18 06:19:31 +0530

    init
    Signed-off-by: Gnanakeethan <gnanakeethan@users.noreply.github.com>

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,10 @@
+
+<style global lang="postcss">
+    @tailwind base;
+    @tailwind components;
+    @tailwind utilities;
+</style>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>Visit <a class='text-blue-600 uppercase' href='https://kit.svelte.dev'>kit.svelte.dev</a> to read the documentation
+</p>
