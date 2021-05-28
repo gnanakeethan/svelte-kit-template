0cea8c6ef6d4e6a250c3ead038131bc138d7b0b2
Gnanakeethan <gnanakeethan@users.noreply.github.com>
2021-05-28 20:23:34 +0530

    Sqash
    Signed-off-by: Gnanakeethan <gnanakeethan@users.noreply.github.com>

--- src/routes/app.svelte
+++ src/routes/app.svelte
@@ -1,2 +1,18 @@
-<a href='/'>Go back</a>
-<p class="text-2xl">Appmaster</p>
+<script>
+    import API, {graphqlOperation} from '@aws-amplify/api';
+    import awsmobile from './../aws-exports.js';
+    import {listSurveyss} from './../graphql/queries';
+    import {onMount} from 'svelte';
+
+    API.configure(awsmobile);
+    onMount(getTodos);
+
+    function getTodos() {
+        return API.graphql(graphqlOperation(listSurveyss)).then(({data}) => {
+            console.log(data);
+        })
+    }
+
+</script>
+
+<div>Testing</div>
--- svelte.config.js
+++ svelte.config.js
@@ -3,22 +3,40 @@ import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: [
-		preprocess({
-			postcss: true,
-		})
-	],
+    // Consult https://github.com/sveltejs/svelte-preprocess
+    // for more information about preprocessors
+    preprocess: [
+        preprocess({
+            postcss: true,
+        })
+    ],
 
 
-	kit: {
-		adapter: adapter({
-			fallback: 'index.html'
-		}),
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
+    kit: {
+        adapter: adapter({
+            fallback: 'index.html'
+        }),
+        // hydrate the <div id="svelte"> element in src/app.html
+        target: '#svelte',
+        vite: {
+            alias: {
+                "tslib": 'tslib/tslib.es6.js',
+            },
+            resolve: {
+                alias: {
+                    "tslib": 'tslib/tslib.es6.js',
+                    "./runtimeConfig": "./runtimeConfig.browser"
+                }
+            },
+            build: {
+                rollupOptions: {
+                    output: {
+                        intro: "if(exports === undefined){var exports ={}; var self = {}}"
+                    }
+                }
+            }
+        }
+    }
 };
 
 export default config;
