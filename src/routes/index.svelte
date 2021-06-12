460a2a157d7fe6ef19bed305f485f26d07aeaaa3
Gnanakeethan <gnanakeethan@users.noreply.github.com>
2021-06-12 11:15:46 +0530

    Update
    Signed-off-by: Gnanakeethan <gnanakeethan@users.noreply.github.com>

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,3 +2,14 @@
 <p>Visit <a class='text-blue-600 uppercase' href='https://kit.svelte.dev'>kit.svelte.dev</a> to read the documentation
 </p>
 <a href='/app'>Go to app</a>
+<script>
+	let count = 0;
+	$: doubled = count * 2;
+	function handleClick() {
+		count += 1;
+	}
+</script>
+
+<button on:click="{handleClick}">Click me!</button>
+
+<p>{count} doubled is {doubled}</p>
